1ca306e6634ab64a470757c1f1ab127afa37da97
Eugene Baraka <barakaeugene17@gmail.com>
2026-06-21 16:31:47 +0300

    Final website updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -165,7 +165,7 @@ export default function Navbar() {
 
                 {/* MOBILE MENU FIXED */}
                 {open && (
-                    <div className="lg:hidden bg-[#081837] border-t border-gray-700">
+                    <div className="lg:hidden bg-[#051026] border-t border-gray-700">
 
                         <div className="px-6 py-6 flex flex-col gap-4 text-white">
 
